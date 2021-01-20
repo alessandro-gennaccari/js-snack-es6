3316de5d7c474ec2cca3d76c8cdeac1a974a8ae5
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-20 14:36:40 +0100

    init

---
+++ snack-20-01-21/js/script.js
@@ -0,0 +1,18 @@
+/* 1.  Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal. */
+
+
+//-------------------------------------------------------------------------------------------------------
+
+
+
+
+
+/* 2.  Partendo da un array creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall'utente
+Usiamo i nuovi metodi degli array foreach o filter.
+Esempio:
+const myArray = ['Pippo', 'Pluto', 'Paperino', 'Paperone', 'Paperina', 'Paperoga'];
+Se l’utente inserisce 2 numeri: 1,4 allora selezioniamo 'Pluto', 'Paperino', 'Paperone', 'Paperina' */
+
+
+//-------------------------------------------------------------------------------------------------------
