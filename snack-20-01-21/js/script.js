7dcc7b045d7c5452c70fd61a428484bf88393496
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-20 15:59:19 +0100

    snack 2 added

--- snack-20-01-21/js/script.js
+++ snack-20-01-21/js/script.js
@@ -53,6 +53,39 @@ Esempio:
 const myArray = ['Pippo', 'Pluto', 'Paperino', 'Paperone', 'Paperina', 'Paperoga'];
 Se l’utente inserisce 2 numeri: 1,4 allora selezioniamo 'Pluto', 'Paperino', 'Paperone', 'Paperina' */
 
+const myArray = ['Pippo', 'Pluto', 'Paperino', 'Paperone', 'Paperina', 'Paperoga'];
 
+let numero1, numero2;
 
+do {
+    numero1 = parseInt(prompt('Inserisci due numeri da 0 a 5\nInserisci il primo numero'))
+}while (numero1 < 0 || numero1 > 5 || isNaN(numero1));
+
+do {
+    numero2 = parseInt(prompt('Inserisci il secondo numero'))
+}while (numero2 < 0 || numero2 > 5 || isNaN(numero2));
+
+// Faccio in modo che per la prossima condizione numero1 sia sempre minore di numero2
+const scambio = numero2;
+if(numero1 > numero2){
+    numero2 = numero1;
+    numero1 = scambio;
+}
+
+// Utilizzando forEach
+// const myNewArray = [];
+// myArray.forEach((e,i) => {
+//     if (i >= numero1 && i <= numero2){
+//         myNewArray.push(e)
+//         console.log(e,i);
+//     }
+// });
+// console.log(myNewArray);
+
+// Utilizzando filter
+const filter = myArray.filter((e,i) => {
+    return i >= numero1 && i <= numero2;
+});
+
+console.log(filter);
 //-------------------------------------------------------------------------------------------------------
